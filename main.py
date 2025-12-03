96fb7e7dfa2160186965def6fd0b08eff6be862d
chahatmandviwala-alt <chahat.mandviwala@gmail.com>
2025-12-03 18:27:31 +0100

    main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ div[data-testid="stTabs"] button p {
 
 /* Optional: reduce gap between tabs a bit */
 div[data-testid="stTabs"] button + button {
-    margin-left: -6px !important;
+    margin-left: -4px !important;
 }
 
 </style>
@@ -1466,5 +1466,6 @@ with tab_tax:
             hide_index=True,
             column_config=final_column_config,
         )
+
 
 
